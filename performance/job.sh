da5ebfc4870b11dbb730fbe6b1ac5974ea96c8a2
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2018-04-08 19:52:42 +0200

    improve metacentrum scripts

--- performance/job.sh
+++ performance/job.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 HOMEDIR="/storage/praha1/home/lucivpav"
-DATADIR="$HOMEDIR/log/"
 trap 'clean_scratch' TERM EXIT
 cd $SCRATCHDIR || exit 1
 
+TMP_DIR="/dev/shm/tmp"
+mkdir $TMP_DIR
+
 module add jdk-8
 RESULTS_FILE="$CONFIG.results"
-JAVA_OPTS=-Xmx3G $HOMEDIR/pack/bin/main $CONFIG > "$RESULTS_FILE"
+JAVA_OPTS="-Xmx8G -Djava.io.tmpdir=$TMP_DIR" $HOMEDIR/pack/bin/main $CONFIG > "$RESULTS_FILE"
 cp "$RESULTS_FILE" $DATADIR || export CLEAN_SCRATCH=false
--- performance/submit.sh
+++ performance/submit.sh
@@ -1,12 +1,22 @@
-BATCH_PATH='../batches/normal.csv'
+BATCH_NAME='normal'
+HOMEDIR="/storage/praha1/home/lucivpav"
+BATCH_PATH="${HOMEDIR}/batches/${BATCH_NAME}.csv"
 TMP_BATCH='tmp.batch'
-OUTPUTS_DIR='outputs'
-cd $OUTPUTS_DIR
+ID="${BATCH_NAME}_$RANDOM"
+OUTPUT_DIR="${HOMEDIR}/outputs/$ID"
+LOGDIR="${HOMEDIR}/log/$ID"
+
+mkdir $OUTPUT_DIR $LOGDIR
+if [[ $? -ne 0 ]] ; then
+	exit 1
+fi
+
+cd $OUTPUT_DIR
 tail -n +2 $BATCH_PATH > $TMP_BATCH
 while read p; do
 	NWORKERS=`echo $p | cut -d' ' -f1`
 	NCPUS=$(($NWORKERS+1))
 	NAME=`echo "$p" | tr ' ' '-'`
-	qsub -N "$NAME" -l select=1:ncpus=$NCPUS:mem=4gb:scratch_local=10gb:cluster=luna -l walltime=0:30:00 -v CONFIG="$p" ../job.sh
+	qsub -N "${NAME}_${ID}" -l select=1:ncpus=$NCPUS:mem=15gb:scratch_local=10gb:cluster=luna -l walltime=0:30:00 -v CONFIG="$p",DATADIR="$LOGDIR" $HOMEDIR/job.sh
 done < $TMP_BATCH
 rm $TMP_BATCH
